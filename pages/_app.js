d1206ed59993dec415e3e26b2dfa1d755f062cba
Cyrus Goh <hello@lovincyrus.com>
2021-07-29 00:12:35 -0700

    add fathom analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
+import { useAnalytics } from '@/lib/analytics'
 import '@/assets/globals.scss'
 import '@/assets/main.scss'
 
 export default function MyApp({ Component, pageProps }) {
+  useAnalytics()
   return <Component {...pageProps} />
 }
